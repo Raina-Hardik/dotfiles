4da544244f3cef899adc2a754f63c1e8d342bbe0
Hardik Raina <hardikraina079@gmail.com>
2026-04-15 22:18:02 +0530

    fix(setopt): remove incorrect syntax for autocomplete setopt

--- dot_config/bash/env/correction-and-completion.sh
+++ dot_config/bash/env/correction-and-completion.sh
@@ -3,13 +3,10 @@
 # shopt -s nocorrect_all          # (equivalent to CORRECT_ALL)
 
 # Misc
-shopt -s interactive_comments    # Allow comments in interactive shell
-
-# Completion
-shopt -s complete_in_word        # Complete from both ends of a word (if available)
+shopt -s interactive_comments # Allow comments in interactive shell
 
 # Navigation
-shopt -s autocd 2>/dev/null      # Type directory name to cd
-shopt -s cdspell 2>/dev/null     # Correct small errors in cd
-shopt -s extglob 2>/dev/null     # Enable extended pattern matching
-shopt -s globstar 2>/dev/null    # Enable recursive ** patterns
+shopt -s autocd 2>/dev/null   # Type directory name to cd
+shopt -s cdspell 2>/dev/null  # Correct small errors in cd
+shopt -s extglob 2>/dev/null  # Enable extended pattern matching
+shopt -s globstar 2>/dev/null # Enable recursive ** patterns
